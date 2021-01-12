7cf4dcb01d5455a92d3d29ce04204c1c11e01994
im <poson_hemp@yahoo.co.jp>
2021-01-12 19:21:42 +0900

    fix get PORT by System.get_env

--- app/config/prod.exs
+++ app/config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :my_app, MyAppWeb.Endpoint,
-  http: [port: {:system, "PORT"}],
+  http: [port: System.get_env("PORT")],
   url: [scheme: "https", host: "containers-no-ecto.herokuapp.com", port: 443] ,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
