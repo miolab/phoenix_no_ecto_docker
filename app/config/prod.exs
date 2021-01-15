871683d6886a1ad6ae266a8cd1fce75ee177066d
im <poson_hemp@yahoo.co.jp>
2021-01-15 19:19:35 +0900

    execute mix format & set MIX_ENV=test case mix test

--- app/config/prod.exs
+++ app/config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :my_app, MyAppWeb.Endpoint,
   http: [port: System.get_env("PORT")],
-  url: [scheme: "https", host: "containers-no-ecto.herokuapp.com", port: 443] ,
+  url: [scheme: "https", host: "containers-no-ecto.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
