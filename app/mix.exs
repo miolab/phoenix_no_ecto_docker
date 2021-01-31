24ebc2a28436534d5d4a65129ae7f3e297d9b770
im <poson_hemp@yahoo.co.jp>
2021-01-31 19:10:01 +0900

    Revert "prepare git_hooks"
    This reverts commit 3781a8b68a2969640a816e1b974727840616f2c1.

--- app/mix.exs
+++ app/mix.exs
@@ -41,8 +41,7 @@ defmodule MyApp.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:git_hooks, "~> 0.5.1", only: [:test, :dev]}
+      {:plug_cowboy, "~> 2.0"}
     ]
   end
 
